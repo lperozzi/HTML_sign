dd2521474a6f2619081f5642cab7f912420b9cc7
Lorenzo Perozzi <lorenzo.perozzi@gmail.com>
2016-05-10 11:38:22 -0400

    update

--- src/signature/conf.js
+++ src/signature/conf.js
@@ -12,6 +12,9 @@ module.exports = [
     linkedin: '<a href="https://ca.linkedin.com/in/lperozzi"><img border="0" alt="linkedin" src="https://politicalfails.files.wordpress.com/2014/07/li-logo.png" width="25" height="25"></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     github: '<a href="https://github.com/lperozzi"><img border="0" alt="linkedin" src="http://cdn.flaticon.com/png/256/25231.png" width="25" height="25"></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     twitter: '<a href="https://twitter.com/lor3nzop3rozzi"><img border="0" alt="linkedin" src="https://g.twimg.com/about/feature-corporate/image/twitterbird_RGB.png" width="22" height="22"></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
-    contactMail: 'lorenzo.perozzi@gmail.com'
+    contactMail: 'lorenzo.perozzi@gmail.com',
+    logoUrl: 'assets/liamg_logo.png',
+    logoAlt: 'liamg logo',
+    website: 'http://www.liamg.ca'
   }
 ]
